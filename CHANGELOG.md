0b68d15b95fe6a04a58b3259d9f8f35f5fe19cec
outdead <outdead@mail.ru>
2022-02-26 23:48:27 +0300

    Fix for 41.66 mp build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+## [v0.7.1] - 2022-02-26
+### Fixed
+- Fix for 41.66 mp build.
+
+### Removed
+- Removed 40 build support.
+
 ## [v0.7.0] - 2021-12-20
 ### Added
 - Added Spanish translation. Thanks to [@SolidShadow](https://steamcommunity.com/profiles/76561198170110257)!
@@ -49,7 +56,8 @@ All notable changes to this project will be documented in this file.
 Slender Doe. 
 - Added EN and RU translations.
 
-[Unreleased]: https://github.com/gorcon/rcon-cli/compare/v0.7.0...HEAD
+[Unreleased]: https://github.com/gorcon/rcon-cli/compare/v0.7.1...HEAD
+[v0.7.1]: https://github.com/gorcon/rcon-cli/compare/v0.7.0...v0.7.1
 [v0.7.0]: https://github.com/gorcon/rcon-cli/compare/v0.6.0...v0.7.0
 [v0.6.0]: https://github.com/gorcon/rcon-cli/compare/v0.5.0...v0.6.0
 [v0.5.0]: https://github.com/gorcon/rcon-cli/compare/v0.4.0...v0.5.0
--- src/lua/client/actions.lua
+++ src/lua/client/actions.lua
@@ -5,80 +5,70 @@
 -- Permanent Effects Drinks adds drinks with a permanent effect
 --
 
-local version = "0.7.0"
+local version = "0.7.1"
 
-local pzversion = string.sub(getCore():getVersionNumber(), 1, 2)
+local pzversion = getCore():getVersionNumber()
 
 -- SlenderDoeSetWeight contains the weight value that will be set to
 -- the character after applying the drink.
 local SlenderDoeSetWeight = 80
 
 -- PerkLevelup creates level up for perk.
-function PerkLevelup(player, perkType, addGlobalXP)
+function PerkLevelup(player, perkType)
     local perkLevel = player:getPerkLevel(perkType);
 
     if perkLevel < 10 then
-        local perk = PerkFactory.getPerk(perkType);
-        local amount = perk:getXpForLevel(perkLevel + 1);
+        if pzversion == "41.65" then
+            local perk = PerkFactory.getPerk(perkType);
+            local amount = perk:getXpForLevel(perkLevel + 1);
 
-        player:getXp():AddXP(perkType, amount, false, false, false, true);
+            player:getXp():AddXP(perkType, amount, false, false, false, true);
 
-        if not perk:isPassiv() and pzversion == "40" then
-            -- if addGlobalXP is false current skill points will be used.
-            if addGlobalXP then
-                -- add XP only for one level.
-                local playerLevel = player:getXp():getLevel()
-
-                -- add 1 to xp to increase the global level. Otherwise the level does not increase,
-                -- and the amount of experience required for the next level takes the value 0.
-                local xpForNextLevel = toInt(player:getXpForLevel(playerLevel) - player:getXp():getTotalXp()) + 1;
-
-                player:getXp():addGlobalXP(xpForNextLevel);
-            end
-
-            -- create level up and recalculate XP to next level.
-            player:LevelPerk(perkType);
-            player:getXp():setXPToLevel(perkType, player:getPerkLevel(perkType));
+            return
         end
+
+        player:LevelPerk(perkType, false);
+        player:getXp():setXPToLevel(perkType, player:getPerkLevel(perkType));
+        SyncXp(player)
     end
 end
 
 -- DrinkHastyHerring adds action to drink Permanent.HastyHerring.
 function DrinkHastyHerring(items, result, player)
-    PerkLevelup(player, Perks.Sprinting, true);
+    PerkLevelup(player, Perks.Sprinting);
 end
 
 -- DrinkDoubleHastyHerring adds action to drink Permanent.DoubleHastyHerring.
 function DrinkDoubleHastyHerring(items, result, player)
-    PerkLevelup(player, Perks.Sprinting, true);
-    PerkLevelup(player, Perks.Fitness, false);
+    PerkLevelup(player, Perks.Sprinting);
+    PerkLevelup(player, Perks.Fitness);
 end
 
 -- DrinkGreedyHammer adds action to drink Permanent.GreedyHammer.
 function DrinkGreedyHammer(items, result, player)
-    PerkLevelup(player, Perks.Blunt, true);
+    PerkLevelup(player, Perks.Blunt);
 end
 
 -- DrinkDoubleGreedyHammer adds action to drink Permanent.DoubleGreedyHammer.
 function DrinkDoubleGreedyHammer(items, result, player)
-    PerkLevelup(player, Perks.Blunt, true);
-    PerkLevelup(player, Perks.Strength, false);
+    PerkLevelup(player, Perks.Blunt);
+    PerkLevelup(player, Perks.Strength);
 end
 
 -- DrinkGreedyAxe adds action to drink Permanent.GreedyAxe.
 function DrinkGreedyAxe(items, result, player)
-    PerkLevelup(player, Perks.Axe, true);
+    PerkLevelup(player, Perks.Axe);
 end
 
 -- DrinkDoubleGreedyAxe adds action to drink Permanent.DoubleGreedyAxe.
 function DrinkDoubleGreedyAxe(items, result, player)
-    PerkLevelup(player, Perks.Axe, true);
-    PerkLevelup(player, Perks.Strength, false);
+    PerkLevelup(player, Perks.Axe);
+    PerkLevelup(player, Perks.Strength);
 end
 
 -- DrinkStrayBullet adds action to drink Permanent.StrayBullet.
 function DrinkStrayBullet(items, result, player)
-    PerkLevelup(player, Perks.Aiming, true);
+    PerkLevelup(player, Perks.Aiming);
 end
 
 -- DrinkSlenderDoe adds action to drink Permanent.SlenderDoe.
