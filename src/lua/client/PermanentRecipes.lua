c4d5b5e99c7e55ae579801b12a8cb21fd45a2ed8
outdead <outdead@mail.ru>
2023-08-07 23:27:28 +0300

    Add PermanentRecipes

---
+++ src/lua/client/PermanentRecipes.lua
@@ -0,0 +1,22 @@
+--
+-- Copyright (c) 2023 outdead.
+-- Use of this source code is governed by the MIT license
+-- that can be found in the LICENSE file.
+--
+
+PermanentRecipes = {
+    MakeWhiskey = {
+        --animationType = "Shoveling",
+        sound = "PourWaterIntoObject",
+        time = 300,
+        skills = {
+          ["Cooking"] = 3,
+        },
+        usedItems = {
+            ["Base.WhiskeyWaterFull"] = 1,
+        },
+        results = {
+            ["Base.WhiskeyFull"] = 1,
+        }
+    },
+}
