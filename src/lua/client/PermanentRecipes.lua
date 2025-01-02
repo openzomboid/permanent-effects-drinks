1a25534591477bf2f66fbc86a820c96bf8bc5e6e
outdead <outdead@mail.ru>
2025-01-03 01:05:09 +0300

    Add Build 42 support

--- src/lua/client/PermanentRecipes.lua
+++ src/lua/client/PermanentRecipes.lua
@@ -17,6 +17,104 @@ PermanentRecipes = {
                 time = 300,
                 cookingSkill = 3,
                 usedItems = {
+                    ["Base.BottleCrafted"] = 1,
+                    ["Base.Yeast"] = 1,
+                    ["Base.Corn"] = 1,
+                    ["Base.UnusableWood"] = 1,
+                },
+                result = "Base.WhiskeyFull",
+                additionalResults = {},
+            },
+            ["MakeWine"] = {
+                name = "MakeWine",
+                type = "Vanilla",
+                disabled = false,
+                sound = "PourWaterIntoObject",
+                texture = "Item_Wine2Full",
+                time = 300,
+                cookingSkill = 3,
+                usedItems = {
+                    ["Base.BottleCrafted"] = 1,
+                    ["Base.Sugar"] = 1,
+                    ["Base.Grapes"] = 1,
+                },
+                result = "Base.Wine2",
+                additionalResults = {},
+            },
+            ["MakeBeer"] = {
+                name = "MakeBeer",
+                type = "Vanilla",
+                disabled = false,
+                sound = "PourWaterIntoObject",
+                texture = "Item_BeerBottle",
+                time = 300,
+                cookingSkill = 3,
+                usedItems = {
+                    ["Base.BottleCrafted"] = 1,
+                    ["Base.Sugar"] = 1,
+                    ["Base.Corn"] = 1,
+                },
+                result = "Base.BeerBottle",
+                additionalResults = {},
+            },
+        },
+
+        Exclusive = {
+            ["MakeSlenderDoe"] = {
+                name = "MakeSlenderDoe",
+                type = "Exclusive",
+                disabled = false,
+                sound = "PourWaterIntoObject",
+                texture = "media/textures/Item_SlenderDoe.png",
+                time = 300,
+                cookingSkill = 10,
+                usedItems = {
+                    ["Permanent.ExclusiveRecipe"] = 1,
+                    ["Base.Wine2"] = 1,
+                    ["Base.Wine"] = 1,
+                    ["Base.GrapeLeaves"] = 4,
+                    ["Base.Milk"] = 1,
+                    ["Base.Pickles"] = 1,
+                },
+                result = "Permanent.SlenderDoe",
+                additionalResults = {},
+            },
+            ["MakeNicotineOverdose"] = {
+                name = "MakeNicotineOverdose",
+                type = "Exclusive",
+                disabled = false,
+                sound = "PourWaterIntoObject",
+                texture = "media/textures/Item_NicotineOverdose.png",
+                time = 300,
+                cookingSkill = 10,
+                usedItems = {
+                    ["Permanent.ExclusiveRecipe"] = 1,
+                    ["Base.Whiskey"] = 1,
+                    ["Base.Sugar"] = 1,
+                    ["Base.CigaretteSingle"] = 100,
+                    ["Base.Coffee2"] = 5,
+                },
+                result = "Permanent.NicotineOverdose",
+                additionalResults = {},
+            },
+        },
+    }
+}
+
+local pzversion = string.sub(getCore():getVersionNumber(), 1, 2)
+
+if pzversion == "41" then
+    PermanentRecipes.Recipes = {
+        Vanilla = {
+            ["MakeWhiskey"] = {
+                name = "MakeWhiskey",
+                type = "Vanilla",
+                disabled = false,
+                sound = "PourWaterIntoObject",
+                texture = "Item_WhiskeyFull",
+                time = 300,
+                cookingSkill = 3,
+                usedItems = {
                     ["Base.WhiskeyWaterFull"] = 1,
                     ["Base.Yeast"] = 1,
                     ["Base.Corn"] = 1,
@@ -98,8 +196,8 @@ PermanentRecipes = {
                 additionalResults = {},
             },
         },
-    },
-}
+    }
+end
 
 PermanentRecipes.IsEnoughMaterials = function(character, recipe)
     if not recipe then return false end
