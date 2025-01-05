7cbefaf967aaac399c8db5dd33470eca6dce5ab9
outdead <outdead@mail.ru>
2025-01-05 23:36:50 +0300

    Add recipe spawn chance

--- src/lua/client/PermanentZombieLoot.lua
+++ src/lua/client/PermanentZombieLoot.lua
@@ -19,21 +19,17 @@ function PermanentZombieLoot.OnZombieDead(corpse)
 
     local character = getPlayer()
 
-    local zombieX = math.floor(corpse:getX());
-    local zombieY = math.floor(corpse:getY());
-    local zombieIsRunner = false;
-    local zombieKiller = -1;
+    local x = math.floor(corpse:getX());
+    local y = math.floor(corpse:getY());
+    local z = math.floor(corpse:getZ());
+    local isSprinter = false;
     local zombieInventory = corpse:getInventory();
 
     for i = 0, getNumClassFields(corpse) - 1 do
         local field = getClassField(corpse, i)
 
         if tostring(field) == "public boolean zombie.characters.IsoZombie.bRunning" then
-            zombieIsRunner = getClassFieldVal(corpse, field);
-        end
-
-        if tostring(field) == "public int zombie.characters.IsoZombie.lastPlayerHit" then
-            zombieKiller = getClassFieldVal(corpse, field);
+            isSprinter = getClassFieldVal(corpse, field);
         end
     end
 
@@ -50,14 +46,10 @@ function PermanentZombieLoot.OnZombieDead(corpse)
         end
     end
 
-    local msg = "Permanent Log: "
-            .. " zombieX = " .. tostring(zombieX) .. " zombieY = " .. tostring(zombieY)
-            .. " isRunner = " .. tostring(zombieIsRunner) .. " killer = " .. tostring(zombieKiller)
-            .. " roomName = " .. roomName
-    print(msg);
-
     if roomName == "bar" then
-        zombieInventory:AddItems("Permanent.ExclusiveRecipe", 1);
+        if PermanentZombieLoot.CheckChance(25) then
+            zombieInventory:AddItems("Permanent.ExclusiveRecipe", 1);
+        end
     end
 end
 
