eb737fe90058243e89380f9145c466b239594b67
outdead <outdead@mail.ru>
2025-01-31 07:01:20 +0300

    Cleanup

--- src/lua/server/MoonshineOptions.lua
+++ src/lua/server/MoonshineOptions.lua
@@ -1,6 +1,7 @@
 --
--- Copyright (c) 2024 outdead.
--- Use of this source code is governed by the Apache 2.0 license.
+-- Copyright (c) 2023 outdead.
+-- Use of this source code is governed by the Apache 2.0 license
+-- that can be found in the LICENSE file.
 --
 
 function MoonshineStillBuildEnabled(param)
